4881787ab2659b3b0dbcbbfd60fe3b436054c597
ikill69 <iankilloran@yahoo.co.uk>
2024-09-02 22:16:00 +1000

    fix image paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/handsomelittledevil' : '',
     output: 'export',
-   
+    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
 
+const config = require('../../next.config');
 const postsDirectory = join(process.cwd(), "_posts");
 
 export function getPostSlugs() {
@@ -15,7 +16,13 @@ export function getPostBySlug(slug: string) {
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  return { ...data, slug: realSlug, content } as Post;
+  // Replace ${basePath} with next.config.js basePath for git hub pages to work
+  let post = { ...data, slug: realSlug, content } as Post;
+  let postString = JSON.stringify(post);
+  postString = postString.replaceAll(/\$\{basePath\}/gi, config.basePath);
+  post = JSON.parse(postString);
+
+  return post as Post;
 }
 
 export function getAllPosts(): Post[] {
